4621c95b93c9f15ae36809f0f4acbaf94a679709
Liran Cohen <gshirkan@gmail.com>
2015-02-26 15:35:56 +0200

    added switching backgrounds capability

--- src/views/BackGround.js
+++ src/views/BackGround.js
@@ -5,6 +5,7 @@
 import ui.View as View;
 import ui.ImageView as ImageView;
 import parallax.Parallax as Parallax;
+import animate;
 
 var config = [
 	{
@@ -37,23 +38,53 @@ exports = Class(View, function (supr) {
 
 		supr(this, 'init', [opts]);
 
-		this.parallax = new Parallax({parent: this});
-		this.parallax.reset(config);
+		this.parallaxA = new Parallax({parent: this});
+		this.parallaxA.reset(config);
+		this.parallaxB = new Parallax({parent: this});
+		this.parallaxB.reset(config);
+
+		this.parallaxAviewA = this.parallaxA.layerPool._views[0].piecePool._views[0]; //style?
+		this.parallaxAviewB = this.parallaxA.layerPool._views[0].piecePool._views[1];
+		this.parallaxBviewA = this.parallaxB.layerPool._views[0].piecePool._views[0]; //style?
+		this.parallaxBviewB = this.parallaxB.layerPool._views[0].piecePool._views[1];
 
 		this.xPos = 0;
 		this.yPos = 0;
+		this.TRANSFORM_MAP_TIME = 1000; 
 	};
 
-	this.reset = function () {
+	resetMap = function ( parallax ) {
 		var map = randomMap();
 		config[0].pieceOptions[0].id = baseName(map);
 		config[0].pieceOptions[0].image = map;
-		this.parallax.reset(config);
+		parallax.reset(config);
+	};
+
+	this.reset = function () {
+		resetMap(this.parallaxA);
+		resetMap(this.parallaxB);
 	};
 
 	this.update = function (x, y) {
 		this.xPos = (this.xPos + x) % PiuPiuGlobals.winSize.width;
 		this.yPos = (this.yPos + y) % PiuPiuGlobals.winSize.height;
-		this.parallax.update(this.xPos, this.yPos);
+		this.parallaxA.update(this.xPos, this.yPos);
+		this.parallaxB.update(this.xPos, this.yPos);
+	};
+
+	this.replaceMap = function () {
+		if (this.isParallaxAActive) {
+			animate(this.parallaxAviewA).clear().now({opacity : 0}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxAviewB).clear().now({opacity : 0}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxBviewA).clear().now({opacity : 1}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxBviewB).clear().now({opacity : 1}, this.TRANSFORM_MAP_TIME, animate.linear);
+			this.isParallaxAActive = false;
+		} else {
+			animate(this.parallaxAviewA).clear().now({opacity : 1}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxAviewB).clear().now({opacity : 1}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxBviewA).clear().now({opacity : 0}, this.TRANSFORM_MAP_TIME, animate.linear);
+			animate(this.parallaxBviewB).clear().now({opacity : 0}, this.TRANSFORM_MAP_TIME, animate.linear);
+			this.isParallaxAActive = true;
+		}
 	};
 });
